950e2e5cd99c46cabde0dde763edc5eb8478fb1b
chotiwat yosthaisong <chotiwatbest31@gamail.com>
2024-08-08 10:13:58 +0700

    first commit

---
+++ src/app/,มั่ว/footet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footet',
+  standalone: true,
+  imports: [],
+  templateUrl: './footet.component.html',
+  styleUrl: './footet.component.scss'
+})
+export class FootetComponent {
+
+}
